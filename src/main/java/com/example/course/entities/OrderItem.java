d6f54238a57a03c2ba0ee9957a8b304523d1ec6a
pazzi <carlos.pazzianotto@gmail.com>
2023-02-23 16:51:48 -0300

    OrderItem many-to-many assoc  extra attr Part II

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -15,8 +15,8 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
-	
+	private OrderItemPK id = new OrderItemPK();
+	 
 	private Integer quantity;
 	private Double price;
 	
